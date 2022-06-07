90c9e09a6cf647d161b66a5d6779e0a2cfe86f59
Bach Le <lehoangbach7802@gmail.com>
2022-06-07 13:54:51 -0400

    commit tree 9ebef83904db9298d74eda9fb2da21c9812dbc14

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,12 @@
 // Code Keypad Component Here
+
+function Keypad () {
+    return (
+    <div>
+        <input type="password"> 
+            console.log('Entering Password')
+        </input>
+    </div>)
+}
+
+export default Keypad;
